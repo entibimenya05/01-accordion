0dea6619ab7c9a3315f3644910e00baf8c2d04f9
emmanuel <entibimenya03@gmail.com>
2025-02-04 12:24:02 +0000

    lift up state and children props on state management exercise

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import "./styles.css";
 import { useState } from "react";
+import "./index.css";
 
 const faqs = [
   {
@@ -25,25 +25,69 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  //in order to make sure that one item can be open only at once while the others are closed
+  //we move the piece of state from AccortdionItem to the Accordion as shown below
+  const [curOpen, setCurOpen] = useState(null);
+  //null means none of them will be open at the beginning
+  //curOpen will hold num of the currently open item
   return (
     <div classname="accordion">
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} key={i} />
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={el.title}
+          //text={el.text}
+          num={i}
+          key={i}
+        >
+          {/*passing the text as children props*/}
+          {el.text}
+        </AccordionItem>
       ))}
+      {/*example below how we can pass an other Accordion Item using children props*/}
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        title="Test 1"
+        //text={el.text}
+        num={22}
+        key="test 1"
+      >
+        <p>Allow React developer to:</p>
+        <ul>
+          <li>Break Up UI into components</li>
+          <li>Make Components Reusable</li>
+          <li>Place State Efficiently</li>
+        </ul>
+      </AccordionItem>
     </div>
   );
 }
-function AccordionItem({ num, title, text, onClick }) {
-  const [isOpen, setIsOpen] = useState(false);
+//now below lets receive those props:curOpen,onOpen
+function AccordionItem({
+  num,
+  title,
+  //text,
+  onClick,
+  curOpen,
+  onOpen,
+  children,
+}) {
+  //const [isOpen, setIsOpen] = useState(false);
+  const isOpen = num === curOpen;
   function handleToggle() {
-    setIsOpen((isOpen) => !isOpen);
+    // setIsOpen((isOpen) => !isOpen);
+    //to toggle the number, just use onOpen and pass in the number
+    //but to make sure that when you click on - it closes see below,set isOpen to null,otherwise num
+    onOpen(isOpen ? null : num);
   }
   return (
     <div classname={`item${isOpen ? "open" : ""}`} onClick={handleToggle}>
       <p classname="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p classname="title">{title}</p>
       <p classname="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen && <div classname="content-box">{text}</div>}
+      {isOpen && <div classname="content-box">{children}</div>}
     </div>
   );
 }
